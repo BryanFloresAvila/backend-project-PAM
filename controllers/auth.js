36f63c74c77abe2933025b7f8268ec591c516eaa
Bryan <elton.flores.a@gmail.com>
2022-07-19 18:05:37 -0500

    fixed: empty token error

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,6 +48,11 @@ const login = async (req, res) => {
 };
 const verifyTokenAdmin = (req, res) => {
   try {
+    if(!req.headers.authorization) {
+      return res.status(401).json({
+        message: 'No token provided'
+      });
+    }
     const token = req.headers.authorization.split(' ')[1];
     const decoded = verifyToken(token);
     if (!decoded) {
